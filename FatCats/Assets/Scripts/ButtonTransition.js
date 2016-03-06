5497da8995a5328b5835ee09c2993de0c3cb7510
Alex French <frenchal17@grinnell.edu>
2016-03-06 14:45:10 -0600

    Added prototype for UI and transitions

--- FatCats/Assets/Scripts/ButtonTransition.js
+++ FatCats/Assets/Scripts/ButtonTransition.js
@@ -9,5 +9,27 @@ function Update () {
 }
 
 function ChangeScene () {
-	GetComponent.<UnityEngine.SceneManagement.SceneManager>().LoadScene(1);
+
+	switch (this.name) {
+		case 'StartButton':
+			GetComponent.<UnityEngine.SceneManagement.SceneManager>().LoadScene(1);
+			break;
+		case 'CalcButton':
+			GetComponent.<UnityEngine.SceneManagement.SceneManager>().LoadScene(2);
+			break;
+		case 'MapButton':
+			GetComponent.<UnityEngine.SceneManagement.SceneManager>().LoadScene(4);
+			break;
+		case 'NotepadButton':
+			GetComponent.<UnityEngine.SceneManagement.SceneManager>().LoadScene(6);
+			break;
+		case 'NewsButton':
+			GetComponent.<UnityEngine.SceneManagement.SceneManager>().LoadScene(5);
+			break;
+		case 'BackButton':
+			GetComponent.<UnityEngine.SceneManagement.SceneManager>().LoadScene(1);
+			break;
+		default:
+			break;
+	}
 }
